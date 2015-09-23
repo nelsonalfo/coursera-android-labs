325f44ba567c320a594961f759fc6fce29fe9fb7
Nelson Ramirez <nelsonalfo@gmail.com>
2015-09-23 16:05:54 -0430

    lista la segun parte del lab de Intents, activar implicitamente una actividad usando Intent.choser() y modificando el AdroidManifets.xml de la BrowserActivity

--- lab3a_IntentsLab/src/main/java/course/labs/intentslab/ActivityLoaderActivity.java
+++ lab3a_IntentsLab/src/main/java/course/labs/intentslab/ActivityLoaderActivity.java
@@ -82,20 +82,21 @@ public class ActivityLoaderActivity extends Activity {
         
 		// TODO - Create a base intent for viewing a URL
 		// (HINT:  second parameter uses Uri.parse())
-		
-        Intent baseIntent = null;
+
+		Uri webpage = Uri.parse(URL);
+		Intent baseIntent = new Intent(Intent.ACTION_VIEW, webpage);
 		
 		// TODO - Create a chooser intent, for choosing which Activity
 		// will carry out the baseIntent
 		// (HINT: Use the Intent class' createChooser() method)
-		Intent chooserIntent = null;
+		Intent chooserIntent = Intent.createChooser(baseIntent, CHOOSER_TEXT);
         
         
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
         
         
 		// TODO - Start the chooser Activity, using the chooser intent
-
+		startActivity(chooserIntent);
         
 	}
     
