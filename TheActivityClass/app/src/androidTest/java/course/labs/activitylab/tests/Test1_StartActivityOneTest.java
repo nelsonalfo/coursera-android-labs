cda83fe7619ec3dff703455a5e857818e78351b6
Nelson Ramirez <nelsonalfo@gmail.com>
2015-09-23 14:42:27 -0430

    cambios nuevos

--- TheActivityClass/app/src/androidTest/java/course/labs/activitylab/tests/Test1_StartActivityOneTest.java
+++ TheActivityClass/app/src/androidTest/java/course/labs/activitylab/tests/Test1_StartActivityOneTest.java
@@ -7,49 +7,47 @@ import com.robotium.solo.Solo;
 import course.labs.activitylab.ActivityOne;
 
 public class Test1_StartActivityOneTest extends
-		ActivityInstrumentationTestCase2<ActivityOne> {
+        ActivityInstrumentationTestCase2<ActivityOne> {
 
-	private Solo solo;
-	private int timeout = 20000;
-	private int sleep = 1000;
+    private Solo solo;
+    private int timeout = 20000;
+    private int sleep = 1000;
 
-	public Test1_StartActivityOneTest() {
-		super(ActivityOne.class);
-	}
+    public Test1_StartActivityOneTest() {
+        super(ActivityOne.class);
+    }
 
-	public void setUp() throws Exception {
-		solo = new Solo(getInstrumentation(), getActivity());
-	}
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
 
-	protected void tearDown() throws Exception {
-		solo.finishOpenedActivities();
-	}
+    protected void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
 
-	// Execution of StartActivityOneTest
-	public void testRun() {
+    // Execution of StartActivityOneTest
+    public void testRun() {
 
-		// ==================== Section One =====================
-		// Wait for activity: 'course.labs.activitylab.ActivityOne'
-		assertTrue("StartActivityOneTest failed: " + "Section One:"
-				+ "ActivityOne did not correctly load", solo.waitForActivity(
-				course.labs.activitylab.ActivityOne.class, timeout));
+        // ==================== Section One =====================
+        // Wait for activity: 'course.labs.activitylab.ActivityOne'
+        boolean condition = solo.waitForActivity(ActivityOne.class, timeout);
+        assertTrue("StartActivityOneTest failed: Section One: ActivityOne did not correctly load", condition);
 
-		
-		// ==================== Section Two =====================
-		// Check for proper counts
-		assertTrue("StartActivityOneTest failed:" + "Section Two:"
-				+ "onCreate() count was off for ActivityOne",
-				solo.waitForText("onCreate\\(\\) calls: 1"));
-		assertTrue("StartActivityOneTest failed:" + "Section Two:"
-				+ "onStart() count was off for ActivityOne",
-				solo.waitForText("onStart\\(\\) calls: 1"));
-		assertTrue("StartActivityOneTest failed:" + "Section Two:"
-				+ "onResume() count was off for ActivityOne",
-				solo.waitForText("onResume\\(\\) calls: 1"));
-		assertTrue("StartActivityOneTest failed:" + "Section Two:"
-				+ "onRestart() count was off for ActivityOne",
-				solo.waitForText("onRestart\\(\\) calls: 0"));
 
-	}
+        // ==================== Section Two =====================
+        // Check for proper counts
+        boolean condition1 = solo.waitForText("onCreate\\(\\) calls: 1");
+        assertTrue("StartActivityOneTest failed: Section Two: onCreate() count was off for ActivityOne", condition1);
+
+        boolean condition2 = solo.waitForText("onStart\\(\\) calls: 1");
+        assertTrue("StartActivityOneTest failed: Section Two: onStart() count was off for ActivityOne", condition2);
+
+        boolean condition3 = solo.waitForText("onResume\\(\\) calls: 1");
+        assertTrue("StartActivityOneTest failed: Section Two: onResume() count was off for ActivityOne", condition3);
+
+        boolean condition4 = solo.waitForText("onRestart\\(\\) calls: 0");
+        assertTrue("StartActivityOneTest failed: Section Two: onRestart() count was off for ActivityOne", condition4);
+
+    }
 
 }
